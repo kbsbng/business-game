7dfbc81faf82c5a7cdbee4203366be2acfbd4e65
Keshavaprasad B S <kbsbng@gmail.com>
2012-12-16 10:35:14 +0530

    Initial version

---
+++ server.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+
+/*jslint anon:true, sloppy:true, nomen:true*/
+
+process.chdir(__dirname);
+
+/*
+ * Create the MojitoServer instance we'll interact with. Options can be passed
+ * using an object with the desired key/value pairs.
+ */
+var Mojito = require('mojito');
+var app = Mojito.createServer();
+
+// ---------------------------------------------------------------------------
+// Different hosting environments require different approaches to starting the
+// server. Adjust below to match the requirements of your hosting environment.
+// ---------------------------------------------------------------------------
+
+var port = 80;
+
+if (typeof process.env.PORT !== "undefined") {
+    port = process.env.PORT
+}
+module.exports = app.listen();
