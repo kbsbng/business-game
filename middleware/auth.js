302ca5c42d5f753c2000f6e4c2b965b25295f03e
Keshavaprasad B S <kbsbng@gmail.com>
2012-12-20 07:48:36 +0530

    Trying faceplate

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,84 +1,32 @@
-var openid = require('openid');
 var url = require('url');
 var nonLoginPaths = {
-    "/login" : true,
-    "/favicon.ico" : true
+  "/login": true,
+  "/favicon.ico": true
 };
 
-var conf = {
-    "dev" : {
-        "host" : "http://localhost:5000"
-    },
-    "production" : {
-        "host" : "http://business-game.kbsbng.com"
+var nonLoginPatterns = ['^/combo~', '^/static'];
+module.exports = function (req, res, next) {
+  var i, parsedUrl;
+  parsedUrl = url.parse(req.url);
+  if (nonLoginPaths[parsedUrl.pathname] === true) {
+    return next();
+  }
+  for (i = 0; i < nonLoginPatterns.length; i++) {
+    if (parsedUrl.pathname.match(nonLoginPatterns[i]) !== null) {
+      return next();
     }
-};
-var env = "dev";
-if (process.env.NODE_ENV == "production") {
-    env ="production";
-}
-console.log("env: "+env);
-var config = conf[env];
-var nonLoginPatterns = [ '^/combo~', '^/static' ];
-module.exports = 
-    function(req, res, next) {
-        console.log(req.headers);
-        console.log("called login handler: " + req.url);
-        var parsedUrl = url.parse(req.url, true);
-        var i;
-        var relyingParty = new openid.RelyingParty(
-            config.host + "/verify",
-            config.host + "/",
-          false,
-            false,
-            []);
+  }
 
-        console.log(parsedUrl);
-        if(parsedUrl.pathname === "/authenticate") {
-             relyingParty.authenticate(parsedUrl.query['openid-identifier'],
-                 false,
-                 function(error, authUrl) {
-                     if (error) {
-                         res.writeHead(200);
-                         res.end('Authentication failed: ' + error.message);
-                         return;
-                     }
-                     if (!authUrl) {
-                         res.writeHead(200);
-                         res.end('Authentication failed');
-                         return;
-                     }
-
-                     res.writeHead(302, { Location: authUrl });
-                     res.end();
-                 });
-             return;
-        }
-
-        console.log(parsedUrl.pathname);
-        if (nonLoginPaths[parsedUrl.pathname] === true) {
-            return next();
-        }
-        for (i = 0; i < nonLoginPatterns.length; i++) {
-            if (parsedUrl.pathname.match(nonLoginPatterns[i]) !== null) {
-                return next();
-            }
-        }
-        if (parsedUrl.pathname === "/verify") {
-            console.log("Verify called");
-            var result = openid.verifyAssertion(req, function(error, result) {
-                res.writeHead(200);
-                
-                if (error || !result.authenticated) {
-                    res.end("Auth failed: error: "+JSON.stringify(error)+"result: "+JSON.stringify(result));
-                    return;
-                }
-                res.end("Success");
-                return;
-            });
-            return;
-        }
-
-        res.writeHead(302, { Location: config[env] + "/login" });
-        res.end();
+  req.facebook.app(function(app) {
+    req.facebook.me(function(user) {
+      if (!user) {
+        console.log("Facebook token not found: " + req.url);
+        req.url = "/login";
+      }
+      else {
+        console.log("Facebook token found: " + req.url);
+      }
+      return next();
+    });
+  });
 };
