36df56b7927199aae2e0253dc50010c7fa3316ab
Keshavaprasad B S <kbsbng@gmail.com>
2012-12-23 17:10:46 +0530

    Add GameList and GameListFrame

---
+++ mojits/GameList/models/foo.server.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('GameListModelFoo', function(Y, NAME) {
+
+/**
+ * The GameListModelFoo module.
+ *
+ * @module GameList
+ */
+
+    /**
+     * Constructor for the GameListModelFoo class.
+     *
+     * @class GameListModelFoo
+     * @constructor
+     */
+    Y.namespace('mojito.models')[NAME] = {
+
+        init: function(config) {
+            this.config = config;
+        },
+
+        /**
+         * Method that will be invoked by the mojit controller to obtain data.
+         *
+         * @param callback {function(err,data)} The callback function to call when the
+         *        data has been retrieved.
+         */
+        getData: function(callback) {
+            callback(null, { some: 'data' });
+        }
+
+    };
+
+}, '0.0.1', {requires: []});
---
+++ mojits/GameListFrame/controller.server.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('GameListFrame', function(Y, NAME) {
+
+/**
+ * The GameListFrame module.
+ *
+ * @module GameListFrame
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            ac.models.get('GameListFrameModelFoo').getData(function(err, data) {
+                ac.composite.done();
+            });
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-assets-addon', 'mojito-models-addon', 'GameListFrameModelFoo', 'mojito-composite-addon']});
